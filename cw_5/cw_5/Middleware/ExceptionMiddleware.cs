e8c6b122fc0ce1b4cfa413607282e10c24eb052e
Adam Daniluk <s16578@pjwstk.edu.pl>
2020-05-09 10:23:32 +0200

    MIddlers

---
+++ cw_5/cw_5/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using cw_5.DTOs.Responses;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw_5.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }catch(Exception exc)
+            {
+                await HandleExceptionAsync(context, exc);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exc)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            return context.Response.WriteAsync(new ErrorDetails
+            {
+                StatusCode = (int)StatusCodes.Status500InternalServerError,
+                Message = "internal server error"
+            }.ToString());
+        }
+    }
+}
